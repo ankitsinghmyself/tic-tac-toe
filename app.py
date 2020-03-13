3f9f19976174b0e95a043cabb0432bc627133c20
Ankit Singh <43289790+ankitsinghmyself@users.noreply.github.com>
2020-03-14 01:45:36 +0530

    new updates

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from tempfile import mkdtemp
 
 app = Flask(__name__)
 
-app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_FILE_DIR"] = "/temp"
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
